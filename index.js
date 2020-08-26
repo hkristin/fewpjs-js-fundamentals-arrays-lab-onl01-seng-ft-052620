6ba71fe4156adca71d4518c6613f3f44ee76f667
Kristin Howard <hkristin@andrew.cmu.edu>
2020-08-26 11:43:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-const append = ["Pet1", "Pet2", "Pet3"];
-const prepend = ["Pet1", "Pet2", "Pet3"];
-const removeLast = ["Pet1", "Pet2", "Pet3"];
-const removeFirst = ["Pet1", "Pet2", "Pet3"];
+const pets = ["Pet1", "Pet2", "Pet3"];
 
-append.push("Odie");
-prepend.unshift("Odie");
-removeLast.pop();
-removeFirst.shift();
+
+pets.push("Odie");
+pets.unshift("Odie");
+pets.pop();
+pets.shift();
